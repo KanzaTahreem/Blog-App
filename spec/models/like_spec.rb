6fe33900144a11cff637291ab962d5c6b01c85d9
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-21 06:19:15 +0500

    Fix rubocop errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,26 +2,29 @@ require_relative '../rails_helper'
 
 RSpec.describe Like, type: :model do
   let(:user) { User.new(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends', posts_counter: 0) }
-  let(:post) { Post.new(title: 'The Great Adventure', text: 'The story of Tom and Jerry', author: user, comments_counter: 0, likes_counter: 0) }
+  let(:post) do
+    Post.new(title: 'The Great Adventure', text: 'The story of Tom and Jerry', author: user, comments_counter: 0,
+             likes_counter: 0)
+  end
   subject { Like.new(author: user, post: post) }
-  
-  before {subject.save }
 
-  it "is valid with valid attributes" do
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid without an author" do
+  it 'is not valid without an author' do
     subject.author = nil
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a post" do
+  it 'is not valid without a post' do
     subject.post = nil
     expect(subject).to_not be_valid
   end
 
-  it "increments the likes_counter of the associated post after saving" do
+  it 'increments the likes_counter of the associated post after saving' do
     expect { subject.save }.to change { post.reload.likes_counter }.by(1)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,55 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  author =  User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
-  subject { Post.new(title: 'Physics', text: 'This is not my first post', comments_counter: 10, likes_counter: 10, author: author) } 
+  author = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends')
+  subject do
+    Post.new(title: 'Physics', text: 'This is not my first post', comments_counter: 10, likes_counter: 10,
+             author: author)
+  end
 
   before { subject.save }
 
-  it "title should be present" do
+  it 'title should be present' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
 
-  it "title should not be too short" do
+  it 'title should not be too short' do
     subject.title = 'a'
     expect(subject).to_not be_valid
   end
 
-  it "title should not be too long" do
+  it 'title should not be too long' do
     subject.title = 'a' * 300
     expect(subject).to_not be_valid
   end
 
-  it "title should be b/w 3 and 250 words" do
+  it 'title should be b/w 3 and 250 words' do
     subject.title = 'a' * 5
     expect(subject).to be_valid
   end
 
-  it "title should not be empty" do
+  it 'title should not be empty' do
     subject.title = ' '
     expect(subject).to_not be_valid
   end
 
-  it "title should have valid value" do
+  it 'title should have valid value' do
     expect(subject.title).to eql 'Physics'
   end
 
-  it "text should be present" do
+  it 'text should be present' do
     subject.text = nil
     expect(subject).to_not be_valid
   end
 
-  it "text should have valid value" do
+  it 'text should have valid value' do
     expect(subject.text).to eql 'This is not my first post'
   end
 
-  it "comments_counter must be integer" do
+  it 'comments_counter must be integer' do
     subject.comments_counter = 12
     expect(subject).to be_valid
   end
 
-  it "comments_counter should have valid value" do
+  it 'comments_counter should have valid value' do
     subject.comments_counter = 12.2
     expect(subject).to_not be_valid
   end
@@ -59,12 +62,12 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it "likes_counter must be integer" do
+  it 'likes_counter must be integer' do
     subject.likes_counter = 12
     expect(subject).to be_valid
   end
 
-  it "likes_counter should have valid value" do
+  it 'likes_counter should have valid value' do
     subject.likes_counter = 12.2
     expect(subject).to_not be_valid
   end
@@ -74,7 +77,7 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it "should return 5 recent comments" do
+  it 'should return 5 recent comments' do
     expect(subject.recent_comments).to eq(subject.comments.order(created_at: :desc).limit(5))
   end
 end
