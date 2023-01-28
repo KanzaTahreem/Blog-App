8525ffebd7ce1e8e6e047392ca1d855e9582fc06
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-28 05:00:30 +0500

    Update the show action of post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :asc)
   end
-  
-  def show; end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
 end
