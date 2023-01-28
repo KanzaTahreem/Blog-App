9a1fb5de0e6824626e6aaba105e86b4b3d826dc7
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-29 04:26:28 +0500

    Allow user to add a new comment to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,23 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params) 
+    @comment.author = current_user
+    @comment.post = Post.find(params[:post_id])
+
+    respond_to do |format|
+      if @comment.save
+
+        redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+      else
+         render :new
+      end
+    end
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
