cfd8908235ff8eb1d79ea6b6adbd568a8e6f6368
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-31 07:43:16 +0500

    Install capybara and database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,9 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+# require 'support/database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
